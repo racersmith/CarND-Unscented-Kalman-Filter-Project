e0dad2ac2d82e3a8eeb7697aaf7e41be57481096
Josh Smith <racersmith@gmail.com>
2017-04-24 13:03:29 -0700

    Add RMSE - Pull RMSE code from my EKF

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+	int est_size = estimations.size();
+
+	// Check for valid vectors
+	if (est_size != ground_truth.size() || est_size == 0) {
+		std::cout << "Invalid vectors passed to CalculateRMSE." << std::endl;
+		std::cout << "  est size: " << est_size << std::endl;
+		std::cout << "  truth size: " << ground_truth.size() << std::endl;
+		return rmse;
+	}
+	else {
+		// Calculate RMSE of valid vectors
+		for (int i = 0; i < est_size; i++) {
+			VectorXd residual = estimations[i] - ground_truth[i];
+			residual = residual.array() * residual.array();
+			rmse += residual;
+		}
+		rmse = rmse / est_size;
+		rmse = rmse.array().sqrt();
+		return rmse;
+	}
 }
